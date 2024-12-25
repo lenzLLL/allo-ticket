cb50bca10636856735a4eae8b0500f5ff090f374
lenz <lenzyounda@gmail.com>
2024-12-25 01:43:43 +0100

    begining travel api connection

--- convex/schema.ts
+++ convex/schema.ts
@@ -55,4 +55,65 @@ export default defineSchema({
   })
     .index("by_user_id", ["userId"])
     .index("by_email", ["email"]),
+    agence:defineTable({
+      name:v.string(),
+      address:v.string(),
+      tel1:v.string(),
+      tel2:v.optional(v.string()),
+      admins:v.optional(v.array(v.string()))
+    }),
+    bus:defineTable({
+      type:v.string(),
+      brand:v.optional(v.string()),
+      agenceId:v.id("agence"),
+    }),
+    seat:defineTable({
+      num:v.string(),
+      busId:v.string(),
+      col:v.string(),
+      row:v.string()
+    }),
+    travel:defineTable({
+      depart:v.optional(v.number()),
+      arrivate:v.optional(v.number()),
+      agenceId:v.id("agence"),
+      class: v.union(
+        v.literal("VIP"),
+        v.literal("SEMI-VIP"),
+        v.literal("CLASSIQUE"),
+        v.literal("BUSINESS")
+      ),
+      wifi:v.boolean(),
+      foods:v.boolean(),
+      electricity:v.boolean(),
+      
+      
+    }),
+    ticketst:defineTable({
+      num:v.string(),
+      userId:v.optional(v.string()),
+      amount:v.string(),
+      paymentIntentId: v.optional(v.string()),
+      status:v.union(
+        v.literal("waiting"),
+        v.literal("valid"),
+        v.literal("used"),
+        v.literal("refunded"),
+        v.literal("cancelled")
+      ), 
+      is_cancelled: v.optional(v.boolean()),
+      totalTickets:v.number(),
+      price:v.number()
+    }),
+    waitingListT: defineTable({
+      eventId: v.id("events"),
+      userId: v.string(),
+      status: v.union(
+        v.literal("waiting"),
+        v.literal("offered"),
+        v.literal("purchased"),
+        v.literal("expired")
+      ),
+      offerExpiresAt: v.optional(v.number()),
+    })
 });
